cdbc306ac1eee7f87fc28a450f5ed97444293b32
Julio-Silva-1973 <jcps0710@gmail.com>
2024-06-03 13:17:31 -0300

    upi

--- api/app.py
+++ api/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/comerciantes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'supersecretkey'
-
 db = SQLAlchemy(app)
+
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
